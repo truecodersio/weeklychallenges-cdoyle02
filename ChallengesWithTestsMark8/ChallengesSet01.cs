8f783f96a103a40d84544c564664bcc5af57fd35
Charlie Doyle <charlesdoyle002@gmail.com>
2019-09-10 13:55:13 -0500

    finished first set

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,37 +6,64 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if(num1 == num2)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            var Sum = minuend - subtrahend;
+            return Sum; 
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var Sum = number1 + number2;
+
+            return Sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if(number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            var result = factor1 * factor2;
+            return result;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                var s = "Hello" + ", " + nameOfPerson + "!";
+                return s;
+            }
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            var s = "HEY!";
+            return s;
         }
     }
 }
